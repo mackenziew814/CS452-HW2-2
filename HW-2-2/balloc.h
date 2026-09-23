6d6f046fa5da615833a180f47a9cb6aede9e62e2
mackenziew814 <mackenziewright@u.boisestate.edu>
2026-09-23 21:33:57 +0000

    Documented balloc.h

--- HW-2-2/balloc.h
+++ HW-2-2/balloc.h
@@ -3,13 +3,20 @@
 
 typedef void *Balloc;
 
+// Create a memory pool
 extern Balloc bcreate(unsigned int size, int l, int u);
+//Delete the memory pool
 extern void   bdelete(Balloc pool);
 
+// Allocate a memory block from the pool
 extern void *balloc(Balloc pool, unsigned int size);
+// Free a memory block
 extern void  bfree(Balloc pool, void *mem);
 
+// Get the size of a memory block
 extern unsigned int bsize(Balloc pool, void *mem);
+
+// Print the state of the memory pool
 extern void bprint(Balloc pool);
 
 #endif
